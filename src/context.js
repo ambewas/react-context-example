d8b22e39c9db218e5cb40a7eba27cc6e0bcffde4
ambewas <ambewas@users.noreply.github.com>
2018-07-04 13:00:42 +0200

    remove default value

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const { Provider, Consumer } = createContext("hello");
+const { Provider, Consumer } = createContext();
 
 export { Provider, Consumer };
--- src/provider.js
+++ src/provider.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Provider } from "./context";
 
-const ContextProvider = ({ children }) => <Provider>{children}</Provider>;
+const ContextProvider = ({ children }) => (
+  <Provider value={"hello world"}>{children}</Provider>
+);
 
-// const ContextProvider = ({ children }) => (
-//   <Provider value={"hello"}>{children}</Provider>
-// );
 export default ContextProvider;
